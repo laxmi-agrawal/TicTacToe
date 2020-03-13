3c5fdcd9722a38d7bd97c593802790be9da7670e
Laxmi Agrawal <laxmi.agrawal88@gmail.com>
2020-03-12 19:39:52 -0700

    added unit test for quesryparser method

---
+++ src/test/java/QueryParserTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.HashMap;
+
+public class QueryParserTest {
+    @Test
+    public void testQueryParser(){
+        Assertions.assertEquals(  null, Handlers.queryParser("a=b"));
+
+        Assertions.assertEquals(  new HashMap<String, String>() {{
+            put("a", "b");
+        }}, Handlers.queryParser("?a=b"));
+
+        Assertions.assertEquals(  new HashMap<String, String>() {{
+            put("abc", "1");
+            put("def","2");
+        }}, Handlers.queryParser("?abc=1&def=2"));
+
+        Assertions.assertEquals(  new HashMap<String, String>() {{
+            put("a", "");
+        }}, Handlers.queryParser("?a="));
+
+    }
+}
